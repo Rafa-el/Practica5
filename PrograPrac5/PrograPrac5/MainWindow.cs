8dfb4b09af7cfba1b987b557bcd83ba7a7f62baa
rahp_27@hotmail.com <rahp_27@hotmail.com>
2013-10-24 20:13:04 -0500

    Programa con interfaz grafica que convierte de pesos a dolares.

---
+++ PrograPrac5/PrograPrac5/MainWindow.cs
@@ -0,0 +1,52 @@
+using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{	
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+	
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+	protected void clicked (object sender, EventArgs e)
+	{
+
+		double pes;
+		double cam;
+		double res;
+		double.TryParse (pesos.Text, out pes);
+		double.TryParse (camb.Text, out cam);
+
+		if (double.TryParse (pesos.Text, out pes) && double.TryParse (camb.Text, out cam)) {
+			if(pes == 0 || cam == 0){
+				MessageDialog cero;
+				cero = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Ok, "Conversion invalida entre cero");
+				cero.Run();
+				cero.Destroy();
+		}
+			else{
+				res = pes / cam;
+				dol.Text = Convert.ToString (res);
+			}
+		} 
+		else if(pesos.Text == "" || camb.Text == ""){
+			MessageDialog d;
+			d = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Ok, "Campos vacios, ingrese numeros");
+			d.Run();
+			d.Destroy();
+		}else {
+			MessageDialog dia;
+			dia = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Ok, "Ingrese solo numeros");
+			dia.Run();
+			dia.Destroy();
+		}
+		
+
+	}
+
+}
